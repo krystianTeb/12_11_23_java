7721df44d69bf015941e97291a962ebfb4a63ac5
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 10:24:50 +0100

    listakrokowAlgorytmy

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int liczbaCalkowita = 5; // 1. Deklaracja zmiennej int o wartości 5
 
-        Scanner scan = new Scanner(System.in);
-
-        while(true) {
-            System.out.println("Podaj Liczbę: ");
-            int liczba = scan.nextInt();
-            System.out.println(liczba % 2 == 0 ?
-                    "Liczba: " + liczba + " jest parzysta." :
-                    "Liczba: " + liczba + " jest nieparzysta.");
+        if(liczbaCalkowita % 5 == 0){           // 2. Sprawdza czy w zmiennej liczbaCalkowita mamy liczbę podzielną przez 5
+            System.out.println(liczbaCalkowita); // 2.1. Jeśli liczba podzielna przez 5 to wypisze zawartość zmiennej.
+        }else{                                  // 3. W przeciwnym wypadku jeśli inna zawartość zmiennej.
+            System.out.println(liczbaCalkowita);// 3.2. Wypisuje zawartość zmiennej jeśli niepodzielna przez 5
         }
 
+        /*
+         * 1. Deklaracja zmiennej int o wartości 5
+         * 2. Sprawdza czy w zmiennej liczbaCalkowita mamy liczbę podzielną przez 5
+         * 2.1. Jeśli liczba podzielna przez 5 to wypisze zawartość zmiennej.
+         * 3. W przeciwnym wypadku jeśli inna zawartość zmiennej.
+         * 3.2. Wypisuje zawartość zmiennej jeśli niepodzielna przez 5
+         * */
 
 
 
