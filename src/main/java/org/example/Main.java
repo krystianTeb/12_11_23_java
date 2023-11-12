863555c500b6d4ee0a8de28f2f0788d271aaa898
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 10:57:58 +0100

    PrzykladyTablice3_generowanie wartosci do tab

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tab[] = new int[4];
-        tab[0] = 1;
-        tab[1] = 2;
-        tab[2] = 3;
-        tab[3] = 4;
+        int tab[] = new int[100];
 
-        for(int val: tab){ // pod val zostają podstawiane wszystkie elementy w tablicy.
+        for(int i = 0; i < 100; i++){ //Pętla generująca wartości do tablicy
+            tab[i] = i;
+        }
+
+        for(int val: tab){ // Pętla wyświetlająca zawartość tab. Pod val zostają podstawiane wszystkie elementy w tablicy.
             System.out.println(  val );
         }
 
