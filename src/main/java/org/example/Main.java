570e30c54023361dbe6a1414c02e91ac7585dbae
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-12 10:55:41 +0100

    PrzykladyTablice2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,18 +4,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tab[] = {3, 6, 6, 8};
-
-        System.out.println( "Długość tab: " + tab.length ); //zwróci długość tablicy (liczba elementów w tablicy)
-        System.out.println( "Pierwszy element: " + tab[0] ); // Odwołanie do pierwszego elementu tablicy (pod indeks 0 jest wartość 3)
+        int tab[] = new int[4];
+        tab[0] = 1;
+        tab[1] = 2;
+        tab[2] = 3;
+        tab[3] = 4;
 
         for(int val: tab){ // pod val zostają podstawiane wszystkie elementy w tablicy.
             System.out.println(  val );
         }
 
-        for(int i = 0; i < tab.length; i++){
-            System.out.println("Indeks: " + i + " pod tym indeksem jest wartość: " + tab[i]);
-        }
+
 
     }
 }
