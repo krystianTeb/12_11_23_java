cb634c51515f14a4922c2a17a8c19aca451b9d57
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 13:36:33 +0100

    22__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Tworzenie obiektu scan do pobierania danych:
-        Scanner scan = new Scanner(System.in);
+        int tab[] = new int[4];
 
-       boolean zm = true;
+        tab[0] = 5;
+        tab[1] = 4;
+        tab[2] = 3;
+        tab[3] = 2;
 
-        //1 sposób
-       String imie = "Ala";
-       System.out.println( imie + " " + imie );
-
-        //2 sposób
-        String imie2 = "Ala ";
-        System.out.println( imie2 + imie2 );
+        for(int i : tab){
+            System.out.println(i);
+        }
 
 
 
