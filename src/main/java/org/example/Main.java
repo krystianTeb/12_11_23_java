12ce10e50ea7452b72617cc92b5e060b1ac3d9d1
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 09:36:17 +0100

    4__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       /* Program wypisujący coś na ekranie.
-          W pierwszej kolejności program prosi o podanie imienia.
-          Program przyjmuje od użytkownika w konsoli tekst.
-          Program wyświetla określony tekst podstawiając wartość pobraną od użytkownika.
-       */
-        /*
-        * 1. Tworzy obiekt skan służący do przyjmowania danych od użytkownika
-        * 2. Wyświetla komunikat w konsoli o treści "Podaj swoje imię:
-        * 3. Pobiera imię w konsoli od użytkownika i zapisuje pod nowo utworzoną zmienną imie.
-        * 4. Wyświetla w konsoli komunikat podstawiając pobrane wcześniej imię.
-         * */
+        //Blok START
+        Scanner scan = new Scanner(System.in);
 
-        Scanner scan = new Scanner(System.in); //1.
-        System.out.println("Podaj swoje imię: ");//2.
-        String imie = scan.next();//3.
-
-        System.out.println("Cześć " + imie + "!\nWitaj w systemie!");//4.
-
+        //Trzy bloki wejścia wyjścia:
+        System.out.println("Podaj a: [Zatwierdź ENTEREM]");
+        int a = scan.nextInt();
+        System.out.println("Podaj b: [Zatwierdź ENTEREM]");
+        int b = scan.nextInt();
+        System.out.println("Podaj c: [Zatwierdź ENTEREM]");
+        int c = scan.nextInt();
+        //Blok operacji:
+        int suma = a + b + c;
+        //Blok wejścia/wyjścia:
+        System.out.println("Wynik sumy: " + a + " + " + b + " + " + c + " = " + suma );
+        //Blok STOP
 
     }
 }
