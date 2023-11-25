c53afd74f5d64855a9600a88ec23ad60663fcdfa
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 08:55:13 +0100

    1__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tab[] = new int[100];
 
-        for(int i = 0; i < 100; i++){ //Pętla generująca wartości do tablicy
-            tab[i] = i+1;
-        }
-
-        for(int val: tab){ // Pętla wyświetlająca zawartość tab. Pod val zostają podstawiane wszystkie elementy w tablicy.
-            System.out.println(  val );
-        }
+        System.out.println("...");
 
 
 
