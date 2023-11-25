3d9e494daf1bfe3ed38f51fe003b9107cdc49dd6
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 12:15:46 +0100

    19__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,22 +8,9 @@ public class Main {
         //Tworzenie obiektu scan do pobierania danych:
         Scanner scan = new Scanner(System.in);
 
-        while (true) {
-            System.out.println("Podaj liczbę 1: [Zatwierdź ENTEREM]");
-            int liczba1 = scan.nextInt();
-            System.out.println("Podaj liczbę 2: [Zatwierdź ENTEREM]");
-            int liczba2 = scan.nextInt();
-            System.out.println("Podaj operator arytmetyczny (+, -, *, /): [Zatwierdź ENTEREM]");
-            String operator = scan.next();
-
-            switch (operator) {
-                case "+" -> System.out.println(liczba1 + " + " + liczba2 + " = " + (liczba1 + liczba2));
-                case "-" -> System.out.println(liczba1 + " - " + liczba2 + " = " + (liczba1 - liczba2));
-                case "*" -> System.out.println(liczba1 + " * " + liczba2 + " = " + (liczba1 * liczba2));
-                case "/" -> System.out.println(liczba1 + " : " + liczba2 + " = " + (liczba1 / liczba2));
-                default -> System.out.println("Podałeś operator: " + operator + "Nie rozpoznaje tej operacji.");
-            }
-        }
+       int i = 5;
+       i *= 2; // i = i * 2;
+       System.out.println(i);
 
 
 
