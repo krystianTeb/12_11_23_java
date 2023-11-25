a27a7455ab1c1cb450ab55b1a5d9d91acdb003a2
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 13:37:48 +0100

    23__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tab[] = new int[4];
+        int tab[] = new int[4];//Deklaracja tablicy i określenie długości (4 elementy - długość)
 
-        tab[0] = 5;
-        tab[1] = 4;
-        tab[2] = 3;
-        tab[3] = 2;
+        tab[0] = 5;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
+        tab[1] = 4;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
+        tab[2] = 3;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
+        tab[3] = 2;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
 
+        //pętla wyświetla wszystko z tablicy (iteruje po elementach tablicy)
         for(int i : tab){
             System.out.println(i);
         }
