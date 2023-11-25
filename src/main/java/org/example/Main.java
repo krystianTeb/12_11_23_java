23289e0497b2fde28dbf23a447c1ba8934f82710
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 09:23:34 +0100

    3__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,12 +10,18 @@ public class Main {
           Program przyjmuje od użytkownika w konsoli tekst.
           Program wyświetla określony tekst podstawiając wartość pobraną od użytkownika.
        */
+        /*
+        * 1. Tworzy obiekt skan służący do przyjmowania danych od użytkownika
+        * 2. Wyświetla komunikat w konsoli o treści "Podaj swoje imię:
+        * 3. Pobiera imię w konsoli od użytkownika i zapisuje pod nowo utworzoną zmienną imie.
+        * 4. Wyświetla w konsoli komunikat podstawiając pobrane wcześniej imię.
+         * */
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj swoje imię: ");
-        String imie = scan.next();
+        Scanner scan = new Scanner(System.in); //1.
+        System.out.println("Podaj swoje imię: ");//2.
+        String imie = scan.next();//3.
 
-        System.out.println("Cześć " + imie + "!\nWitaj w systemie!");
+        System.out.println("Cześć " + imie + "!\nWitaj w systemie!");//4.
 
 
     }
