4c8868e09a6f8b95e29ce48188f19a8c03d4f293
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 09:19:28 +0100

    2__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+       /* Program wypisujący coś na ekranie.
+          W pierwszej kolejności program prosi o podanie imienia.
+          Program przyjmuje od użytkownika w konsoli tekst.
+          Program wyświetla określony tekst podstawiając wartość pobraną od użytkownika.
+       */
 
-        System.out.println("...");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Podaj swoje imię: ");
+        String imie = scan.next();
 
+        System.out.println("Cześć " + imie + "!\nWitaj w systemie!");
 
 
     }
