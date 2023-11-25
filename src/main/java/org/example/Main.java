96cc391869c8ab038020e3fee2e9428a38bf70e1
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 13:41:49 +0100

    24__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tab[] = new int[4];//Deklaracja tablicy i określenie długości (4 elementy - długość)
+        int tab[] = new int[400];//Deklaracja tablicy i określenie długości (4 elementy - długość)
 
-        tab[0] = 5;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
-        tab[1] = 4;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
-        tab[2] = 3;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
-        tab[3] = 2;//inicjalizacja wartości w tablicy (przypisanie wartości do indeksów)
+        for(int i = 0; i < tab.length; i++){
+            tab[i] = i;
+        }
 
         //pętla wyświetla wszystko z tablicy (iteruje po elementach tablicy)
         for(int i : tab){
