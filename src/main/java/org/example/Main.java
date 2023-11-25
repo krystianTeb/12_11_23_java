eb01f83b7ffa0f57d50f8ec14c9a0c794f79ca27
Krystian <krystian.lewandowski@teb.edu.pl>
2023-11-25 11:15:10 +0100

    13__25_11_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,18 +8,13 @@ public class Main {
         //Tworzenie obiektu scan do pobierania danych:
         Scanner scan = new Scanner(System.in);
 
-        while(true) {
-            System.out.println("Podaj liczbę do weryfikacji: [Zatwierdź ENTEREM]");
-            int liczba = scan.nextInt();
-
-            System.out.println( liczba % 2 == 0 ? "Liczba: " + liczba + " jest parzysta!" :
-                                                  "Liczba: " + liczba + " nie jest parzysta!" );
-
-            System.out.println("Aby kontynuować wpisz T ,jeśli zatrzamać wpisz N!");
-            String czyZatrzymac = scan.next();
-            if(czyZatrzymac.equals("N") || czyZatrzymac.equals("n") || czyZatrzymac.equals("nie")) break;
+            //System.out.println("Podaj liczbę do weryfikacji: [Zatwierdź ENTEREM]");
+            //int liczba = scan.nextInt();
 
 
+        for( ; true ; ) {
+
+            System.out.println("Test");
         }
 
 
