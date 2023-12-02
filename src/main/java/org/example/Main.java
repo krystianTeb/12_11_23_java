0e39cdb9c9f67c2c49ef79aa053cd818c102f541
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 16:52:02 +0100

    test49__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-     System.out.println("Nowakowski".compareTo("Nowak"));
-     System.out.println("Nowak".compareTo("Nowakowski"));
+        System.out.println("Ala" + " ma " + "kota");//z plusem jako operator konkatenacji (łączenia ciągów znaków)
+        System.out.println("Ala".concat(" ma ").concat("kota"));//z metodą concat
 
 
 
