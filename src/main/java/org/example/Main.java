ec378122b652022f01aa5f63c690fb1bed47d1e4
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 15:22:39 +0100

    test33__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        String[] produkty = { "Fresca FORTE kawa ziarnista świeżo palona 1kg", "Kawa ziarnista BRAZYLIA 1kg Świeżo Palona 100% ARABICA - Palarnia Kawy"};
-
-
-        for(String produkt : produkty){
-            System.out.println(produkt);
+        int[] elementy = new int[10];
+        for(int i = 0; i < elementy.length; i++){
+            elementy[i] = i + 101;
         }
 
+        for(int element : elementy){
+            System.out.println(element);
+        }
 
     }
 }
