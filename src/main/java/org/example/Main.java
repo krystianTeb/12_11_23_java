57075aeb82d59bbbe55d4ca46d276dad9e5d881b
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 12:41:43 +0100

    test26__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,33 +5,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int tab[] = new int[400];//Deklaracja tablicy i określenie długości (4 elementy - długość)
+        int jednaLiczba = 55; //nazewnictwo zmiennej camelCase
+        int[] tablicaLiczba = {0, 1, 2, 3};
 
-        for(int i = 0; i < tab.length; i++){
-            tab[i] = i;
-        }
-
-        //pętla wyświetla wszystko z tablicy (iteruje po elementach tablicy)
-        for(int i : tab){
-            System.out.println(i);
-        }
-
-        int i = 5,
-            s = 5;
-
-        for(int u = 0; u <= 5; u++){
-            System.out.println(u);
-        }
-        float x = 0.5f;
-        while(x <= 45){
-            System.out.println(x);
-            x+=1.25;
-        }
-
-        Scanner scan = new Scanner(System.in);
-        int liczba = scan.nextInt();
-        System.out.println(liczba);
-        System.out.println("To jest liczba1: " + liczba);
+        System.out.println( "Pierwszy element tablicy: " + tablicaLiczba[0] +
+                            "\nDrugi element tablicy: " + tablicaLiczba[1] +
+                            "\nTrzeci element tablicy: " + tablicaLiczba[2] +
+                            "\nCzwarty element tablicy: " + tablicaLiczba[3]);
 
 
     }
