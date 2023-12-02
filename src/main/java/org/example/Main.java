58f329e11a1fea555ec6509f96df89019261e24d
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 16:06:59 +0100

    test43__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,24 +4,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /* Ćwiczenie 5
-        Napisz program, w którym zostanie utworzona 100-elementowa tablica liczb typu int.
-        Komórkom o indeksach 0, 10, 20, … , 90 przypisz wartość 0, komórkom 1, 11, 21, … , 91
-        wartość 1, komórkom 2, 12, 22, … , 92 wartość 2 itd.
-        */
 
-        int[] liczby = new int[100];
-        for(int i = 0; i < liczby.length; i++){
-            liczby[i] = i % 10; // 1 % 10 reszta: 1    2 % 10 reszta: 2   10 % 10 reszta: 0    11 % 10  reszta: 1
-        }
-        // 0, 10, 20, 30 ... to przypisuje 0
-        // 1, 11, 21, 31 ... to podstawia 1
-        // 2, 12, 22, 32 ... to podstawia 2
-        for(int liczba : liczby){
-            System.out.print(liczba);
-        }
-        System.out.println("\n" + liczby[0] + " " + liczby[10] + " " + liczby[20] + " " + liczby[30]);
+        // Utworzenie 26-elementowej tablicy typu char
+        char[] alphabet = new char[26];
 
-        System.out.println(liczby[1] + " " + liczby[11] + " " + liczby[21] + " " + liczby[31]);
+        // Zapisanie małych liter alfabetu od a do z w tablicy
+        for (int i = 0; i < 26; i++) {
+            alphabet[i] = (char) ('a' + i); // 97 + 0 == 97     2_iteracja: 97 + 1 == 98     3_iteracja:  97 + 2 == 99
+        }
+
+        // Wyświetlenie zawartości tablicy
+        for (char letter : alphabet) {
+            System.out.print(letter + " ");
+        }
     }
 }
