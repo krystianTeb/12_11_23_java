5637912b21a736da80044de79fd178348e42d4a8
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 15:29:49 +0100

    test35__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,21 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*Ćwiczenie 1
-        Napisz program, w którym zostanie utworzona 10-elementowa tablica liczb typu int. Za pomocą pętli for zapisz
-        w kolejnych komórkach liczby od 101 do 110. Zawartość tablicy wyświetl na ekranie.
+
+        /*Ćwiczenie 2
+        Napisz program, w którym zostanie utworzona 10-elementowa tablica liczb typu int. Użyj pętli for do
+        wypełnienia jej danymi w taki sposób, aby w kolejnych komórkach znalazły się liczby od 10 do 100 (czyli 10,
+        20, 30 itd.). Zawartość tablicy wyświetl na ekranie.
         */
-        int[] elementy = new int[10];
-        for(int i = 0; i < elementy.length; i++){
-            elementy[i] = i + 101;
+        int[] liczby = new int[10];
+
+        int wartosc = 10;
+        for (int i = 0; i < liczby.length; i++){
+            liczby[i] = wartosc;
+            wartosc += 10;
         }
 
-        for(int element : elementy){
+        for(int element : liczby){
             System.out.println(element);
         }
 
