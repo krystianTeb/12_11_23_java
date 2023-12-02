b95da20794dad8385de57e27993319b8000a06d3
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 16:49:53 +0100

    test48__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       String imie = "Adam"; //1 sposób zapisu
-       String imie2 = new String("Adam"); //2 sposób zapisu
-       System.out.println(imie + " " + imie2);
-
-       int liczba = 5;
-       Integer liczba2 = 5;
-       System.out.println(liczba + " " + liczba2);
-
+     System.out.println("Nowakowski".compareTo("Nowak"));
+     System.out.println("Nowak".compareTo("Nowakowski"));
 
 
 
