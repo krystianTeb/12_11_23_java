bd8599e218275d1ae2b1d54675f303fb6935268c
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 15:50:10 +0100

    test41__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,20 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-        /*  Ćwiczenie 3 i 4
-            Napisz program, w którym zostanie utworzona 20-elementowa tablica typu boolean. Komórkom o indeksie
-            parzystym przypisz wartość true, a o indeksie nieparzystym - false (zero możesz uznać za wartość parzystą).
-            Zawartość tablicy wyświetl na ekranie.
+        /* Ćwiczenie 5
+        Napisz program, w którym zostanie utworzona 100-elementowa tablica liczb typu int.
+        Komórkom o indeksach 0, 10, 20, … , 90 przypisz wartość 0, komórkom 1, 11, 21, … , 91
+        wartość 1, komórkom 2, 12, 22, … , 92 wartość 2 itd.
         */
-        boolean[] tablica = new boolean[20];
 
-        for (int i = 0; i < tablica.length; i++){
-            tablica[i] = i % 2 == 0 && i != 0;
+        int[] liczby = new int[100];
+        for(int i = 0; i < liczby.length; i++){
+            liczby[i] = i % 10;
         }
 
-        for(boolean element : tablica){
-            System.out.println(element);
+        for(int liczba : liczby){
+            System.out.print(liczba);
         }
 
     }
