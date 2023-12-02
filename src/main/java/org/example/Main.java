189cae9bcffdab94e9630b41a9eb64e39fbeeb7b
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 13:24:37 +0100

    test29__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        int[] zbiorLiczb = new int[4]; //Deklaracja tablicy typu int, która zawiera 4 elementy.
+        int[] zbiorLiczb = new int[150]; //Deklaracja tablicy typu int, która zawiera 4 elementy.
 
         //pętla dodająca do tablicy nowe wartości
         for(int i = 0; i < zbiorLiczb.length; i++){
             zbiorLiczb[i] = i;
         }
-
-        // zbiorLiczb[0] = 0;
-        // zbiorLiczb[1] = 1;
-        // zbiorLiczb[2] = 2;
-        // zbiorLiczb[3] = 3;
 
         //pętla foreach - wypisującą wszystko z tablicy
         for(int element : zbiorLiczb){
