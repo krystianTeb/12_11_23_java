f770907c02f4ec3205f68859390574d43f8e11b8
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-02 15:23:09 +0100

    test34__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        /*Ćwiczenie 1
+        Napisz program, w którym zostanie utworzona 10-elementowa tablica liczb typu int. Za pomocą pętli for zapisz
+        w kolejnych komórkach liczby od 101 do 110. Zawartość tablicy wyświetl na ekranie.
+        */
         int[] elementy = new int[10];
         for(int i = 0; i < elementy.length; i++){
             elementy[i] = i + 101;
