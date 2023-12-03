b438ad3d7eb4032605623a30f3ec9a05d404702e
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 11:18:58 +0100

    test61__03_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,19 +8,12 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        int liczba1, liczba2, liczba3;
 
-        System.out.println("Podaj liczbę 1: ");
-        liczba1 = pobierzLiczbeOdUzytkownika();
-        System.out.println("Podaj liczbę 2: ");
-        liczba2 = pobierzLiczbeOdUzytkownika();
-        System.out.println(liczba1 + " " + liczba2);
-
-        System.out.println("Wartość max: " + Math.max(liczba1, liczba2));
-        System.out.println("Wartość min: " + Math.min(liczba1, liczba2));
+        Powitanie();
+        Powitanie();
+        Powitanie();
     }
-    static int pobierzLiczbeOdUzytkownika(){
-        Scanner scan = new Scanner(System.in);
-        return scan.nextInt();
+    static void Powitanie(){
+       System.out.println("Cześćć :)");
     }
 }
