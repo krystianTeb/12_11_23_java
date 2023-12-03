79f3b359bd295d65d7932de5d143a3fb4621027d
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 11:27:33 +0100

    test63__03_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,11 +9,15 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println( Dodawanie(2, 2) );
-        System.out.println( Dodawanie(5, 2) );
-        System.out.println( Dodawanie(5, 5) );
+        System.out.println( Math.floor( Math.random() * 10) );//generuje wartości od 0 - 9
+        System.out.println( Math.floor( Math.random() * 10) + 1 );//generuje wartości od 1 - 10
+        System.out.println( Math.ceil( Math.random() * 10) );//generuje wartości od 1 - 10
+
+        //Zapisanie jako int
+        System.out.println( (int)Math.floor( Math.random() * 10) );//generuje wartości od 0 - 9
+        System.out.println( (int)Math.floor( Math.random() * 10) + 1 );//generuje wartości od 1 - 10
+        System.out.println( (int)Math.ceil( Math.random() * 10) );//generuje wartości od 1 - 10
+
     }
-    static int Dodawanie(int liczba1, int liczba2){
-        return liczba1 + liczba2;
-    }
+
 }
