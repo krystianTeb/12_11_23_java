581f2c13d7a3947e6e40b2c15cf7f5ae23674df9
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 09:26:35 +0100

    test56__03_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        /*Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         System.out.println("Podaj słowo: ");
-        String slowo = scan.nextLine();*/
+        String zdanie = scan.nextLine();
 
-
-        String ala = " Ala ma kota ";
-        System.out.println(ala);
-        System.out.println(ala.trim());
+        //Ala ma kota.    string[0] = "Ala"   string[1] = "ma"  string[2] = "kota."
+        String[] slowa = zdanie.split(" "); // Podział wprowadzonego tekstu na słowa
+        for(String slowo : slowa){
+            System.out.println(slowo);
+        }
+        System.out.println(slowa[0] + " " + slowa[1] + " " + slowa[2]);
 
 
 
