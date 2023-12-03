8c322501e2930dbb2979029ec59be35cc16a48eb
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 09:51:07 +0100

    test57__03_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,31 @@
 package org.example;
 
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
+/*        Scanner scan = new Scanner(System.in);
         System.out.println("Podaj słowo: ");
-        String zdanie = scan.nextLine();
+        String zdanie = scan.nextLine();*/
 
-        //Ala ma kota.    string[0] = "Ala"   string[1] = "ma"  string[2] = "kota."
-        String[] slowa = zdanie.split(" "); // Podział wprowadzonego tekstu na słowa
-        for(String slowo : slowa){
-            System.out.println(slowo);
+
+        //Jak zmodyfikować pierwszą literę aby była wielka:
+        String testoweSlowo = "test";
+        System.out.println(Character.toUpperCase(testoweSlowo.charAt(0)) + testoweSlowo.substring(1));
+
+        //Palindrom:
+        String str = "Augusta".toLowerCase();
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
+                System.out.println("Słowo nie jest palindromem!"); // Jeśli litery nie są sobie równe, to słowo nie jest palindromem
+                break;
+            }
         }
-        System.out.println(slowa[0] + " " + slowa[1] + " " + slowa[2]);
-
-
 
 
 
