a35573f9cb6bdd23a13bb1dd2422b7930f9e917b
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 11:51:41 +0100

    test66__03_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,35 +5,10 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-     /*  Sposob 1
-     while(true) {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Podaj liczbę z przedziału od 1-6: ");
-            int podanaLiczba = scan.nextInt();
-
-            int wylosowanaLiczba = (int) Math.ceil(Math.random() * 6);
-            System.out.println(podanaLiczba == wylosowanaLiczba ?
-                    "Liczba jest trafiona! \nLiczba: " + podanaLiczba + "\nWylosowane: " + wylosowanaLiczba :
-                    "Liczba NIE jest trafiona! \nLiczba: " + podanaLiczba + "\nWylosowane: " + wylosowanaLiczba);
-        }*/
-
-        /* Sposob 2 */
-
-        while(true) {
-            Scanner scan = new Scanner(System.in);
-            System.out.println("Podaj liczbę z przedziału od 1-6: ");
-            int podanaLiczba = scan.nextInt();
-
-            Random rand = new Random();
-            //int wylosowanaLiczba = rand.nextInt(5); // generuje wartości: 0-5
-            //int wylosowanaLiczba = rand.nextInt(5) + 1; // generuje wartości: 1-6
-            int wylosowanaLiczba = rand.nextInt(1, 7); // generuje wartości: 1-6
-
-            System.out.println(podanaLiczba == wylosowanaLiczba ?
-                    "Liczba jest trafiona! \nLiczba: " + podanaLiczba + "\nWylosowane: " + wylosowanaLiczba :
-                    "Liczba NIE jest trafiona! \nLiczba: " + podanaLiczba + "\nWylosowane: " + wylosowanaLiczba);
-        }
-
+        System.out.println(Math.rint(3.1)); // 3.0
+        System.out.println(Math.rint(3.499)); // 3.0
+        System.out.println(Math.rint(3.5)); // 4.0
+        System.out.println(Math.rint(3.7)); // 4.0
 
 
     }
