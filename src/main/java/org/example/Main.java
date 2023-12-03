6fec7ce532a7a93ac2e059f936be0bbd61ff066f
Krystian <krystian.lewandowski@teb.edu.pl>
2023-12-03 08:50:30 +0100

    test54__02_12_23

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,15 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        System.out.println("Podaj imię: ");
-        String imie = scan.nextLine();
-        imie = imie.toLowerCase();
-        if(imie.equals("ala")) {
-            System.out.println("Podane imię to Ala! ");
-        }
+        System.out.println("Podaj słowo: ");
+        String slowo = scan.nextLine();
+
+        System.out.println("To twoje słowo pisane małymi literami: " + slowo.toLowerCase());
+        System.out.println("To twoje słowo pisane dużymi literami: " + slowo.toUpperCase());
+        //powyższe wywołania metody .toUpperCase i toLowerCase nie zmienia zawartości zmiennej.
+        System.out.println("To twoje słowo pisane oryginalnie: " + slowo);
+
+
 
 
 
